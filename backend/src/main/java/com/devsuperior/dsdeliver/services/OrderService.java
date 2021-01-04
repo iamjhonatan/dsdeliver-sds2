33fb4e3abb72b9fa3a6c765e18396f249baba923
Jhonatan Medeiros <55262464+iamjhonatan@users.noreply.github.com>
2021-01-04 18:22:06 -0300

    POST /orders

--- backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
@@ -1,5 +1,6 @@
 package com.devsuperior.dsdeliver.services;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,8 +9,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dsdeliver.dto.OrderDTO;
+import com.devsuperior.dsdeliver.dto.ProductDTO;
 import com.devsuperior.dsdeliver.entities.Order;
+import com.devsuperior.dsdeliver.entities.OrderStatus;
+import com.devsuperior.dsdeliver.entities.Product;
 import com.devsuperior.dsdeliver.repositories.OrderRepository;
+import com.devsuperior.dsdeliver.repositories.ProductRepository;
 
 //a classe é um componente registrado que podera ser injetado em outros componentes
 @Service 
@@ -18,12 +23,30 @@ public class OrderService {
 	@Autowired
 	private OrderRepository repository;
 	
+	@Autowired
+	private ProductRepository productRepository;
+	
 	@Transactional(readOnly = true)
 	public List<OrderDTO> findAll(){
 		List<Order> list = repository.findOrdersWithProducts();
 		return list.stream()
 				.map(x -> new OrderDTO(x))
 				.collect(Collectors.toList());
+	}
+	
+	// O objeto DTO passado como parametro, vai conter todos os dados do pedido e tambem os produtos
+	// Instanciando um novo pedido. Depois, varrer todos os produtos no DTO.
+	// Depois, instanciar um produto com base no ID do 'p', adicionando o produto instanciado na lista de produtos do order instanciado.
+	// Assim, o order tera os dados basicos e os produtos associados a ele, podendo assim, ser salvo no banco.
+	@Transactional
+	public OrderDTO insert(OrderDTO dto){
+		Order order = new Order(null, dto.getAddres(), dto.getLatitude(), dto.getLongitude(), Instant.now(), OrderStatus.PENDING);
+		for (ProductDTO p : dto.getProducts()) {
+			Product product = productRepository.getOne(p.getId());
+			order.getProducts().add(product);
+		}
+		order = repository.save(order);
+		return new OrderDTO(order);
 	}
 
 }
